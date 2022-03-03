4edc342b3f801558bc90fbe4a186d7a8c1f831ba
abeforgit <arnebertrand@gmail.com>
2022-03-03 18:56:11 +0100

    Only connect tcp client when streaming active

--- LiveStreamer.cs
+++ LiveStreamer.cs
@@ -1,33 +1,45 @@
-using System;
-using System.Collections.Generic;
+using System.Net.Sockets;
 using Godot;
-using Newtonsoft.Json;
 
 namespace Telraam_sim
 {
-	public class LiveStreamer
-	{
-		private static LiveStreamer myInstance;
+    public class LiveStreamer
+    {
+        private static LiveStreamer _myInstance;
+        private TcpClient tcpClient;
 
-		public bool Streaming { get; set; }
+        public bool Streaming { get; set; }
 
-		private LiveStreamer()
-		{
-		}
+        private LiveStreamer()
+        {
+            tcpClient = new TcpClient();
+        }
 
-		public static LiveStreamer GetInstance()
-		{
-			return myInstance ?? (myInstance = new LiveStreamer());
-		}
+        public static LiveStreamer GetInstance()
+        {
+            return _myInstance ?? (_myInstance = new LiveStreamer());
+        }
 
-		public void Start()
-		{
-			Streaming = true;
-		}
+        public void Start()
+        {
+            Streaming = true;
+            tcpClient.Connect("localhost", 4564);
+        }
 
-		public void Stop()
-		{
-			Streaming = false;
-		}
-	}
+        public void Stop()
+        {
+            Streaming = false;
+            tcpClient.Close();
+        }
+
+        public void Send(byte[] message)
+        {
+            tcpClient.GetStream().Write(message, 0, message.Length);
+        }
+
+        public void SendString(string message)
+        {
+            Send(message.ToUTF8());
+        }
+    }
 }
